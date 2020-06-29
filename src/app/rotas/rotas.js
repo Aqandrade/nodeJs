b29c0d998e2a40be8a0b431cf92ebdc7ad4cdb85
Alisson <alisson.andrade@codding.com.br>
2020-06-29 18:23:37 -0300

    Encapsulando o express e rotas, deixando cada responsabilidade separada.

---
+++ src/app/rotas/rotas.js
@@ -0,0 +1,30 @@
+module.exports = (app) =>{
+    
+    app.get('/',function(request,response){
+        console.log(`URL: '/' acessada`);
+        response.send(`
+            <html>
+                <head>
+                    <meta charset='utf-8'/>
+                </head>
+                <body>
+                    <h1>Casa do código</h1>
+                </body>
+            </html>`
+        );
+    });
+    
+    app.get('/listagemLivros',function(request,response){
+        console.log(`URL: '/listagemLivros' acessada`);
+        response.send(`
+            <html>
+                <head>
+                    <meta charset='utf-8'/>
+                </head>
+                <body>
+                    <h1>Listagem livros</h1>
+                </body>
+            </html>
+        `);
+    });
+}
