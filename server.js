c6c60ba10734cd442a5a85440aa936deabb5fcb9
Alisson <alisson.andrade@codding.com.br>
2020-06-29 17:45:27 -0300

    Utilizando Express JS
    const express = require("express"); // retornar uma função do express
    const app = express(); // Retorna o objeto express.
    
    app.listen(3000,function(){//Inicio o servidor na porta 3000 passando uma callback function
        app.get('/',function(request,response){ // Configuro uma requisição get para a rota /
            response.send('<h1>Olá requisição get em /</h1>'); // Envio um título quando a requisição nessa rota for executada.
        });
    });

--- server.js
+++ server.js
@@ -1,4 +1,43 @@
-const http = require('http');
+const express = require('express');
+const { response } = require('express');
+
+const app = express();
+
+app.listen(3000,function(){
+
+    console.log("Servidor iniciado na porta 3000");
+
+    app.get('/',function(request,response){
+        console.log(`URL: '/' acessada`);
+        response.send(`
+            <html>
+                <head>
+                    <meta charset='utf-8'/>
+                </head>
+                <body>
+                    <h1>Casa do código</h1>
+                </body>
+            </html>`
+        );
+    });
+
+    app.get('/listagemLivros',function(request,response){
+        console.log(`URL: '/listagemLivros' acessada`);
+        response.send(`
+            <html>
+                <head>
+                    <meta charset='utf-8'/>
+                </head>
+                <body>
+                    <h1>Listagem livros</h1>
+                </body>
+            </html>
+        `);
+    });
+
+})
+
+/*const http = require('http');
 
 const servidor = http.createServer(function(request,response){
     if(request.url == '/'){
@@ -28,4 +67,4 @@ const servidor = http.createServer(function(request,response){
 
 });
 
-servidor.listen(3000);
+servidor.listen(3000);*/
